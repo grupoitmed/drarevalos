0be5ce114da8df2544aef5bde9e0a1d209635cde
grupoitmed <soporte@grupoitmed.com>
2019-10-06 22:30:54 -0600

    Se agrego el plugin react-native-elements y se desarrollo el login asta el punto de enviar los datos

--- App.js
+++ App.js
@@ -1,19 +1,75 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button,Image } from 'react-native-elements';
+import t from "tcomb-form-native";
+const Form = t.form.Form;
+import { LoginStruct,LoginOptions } from "./app/screen/Login";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  constructor (){
+    super();
+
+    this.state = {
+      formValue : {
+        user:"kjjhh",
+        password:"jjh",
+      }
+    };
+  }
+
+  onChange = formValue => {
+    
+    this.setState({
+      formValue
+    });
+    console.log(this.state);
+  } 
+
+  
+  sendFrom = () => {
+    
+    console.log(this.state);
+  } 
+
+  render(){
+    const { formValue }  = this.state;
+    const logo = require('./assets/images/logo.png');
+    return ( 
+      
+      <View style={styles.container}>
+          <View style={styles.containerImage}>
+          <Image
+              source={logo}
+              style={styles.logo}
+            />
+        </View>
+          <Form 
+            ref="formValue"
+            type={LoginStruct}
+            options={LoginOptions}
+            value={formValue}
+            onChange={v => this.onChange(v)}
+          />
+          <Button buttonStyle={styles.button} title="Login" onPress={this.sendFrom.bind(this)} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+  },
+  containerImage:{
+    alignItems: 'center'
+  },
+  button:{
+    margin:20,
+  },
+  logo: {
+      width: 250,
+      height: 250
   },
 });
